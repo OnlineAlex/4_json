4711682a1a0a615dd5d18d741ca21660e86f7f8f
OnlineAlex <becasualua@gmail.com>
2018-03-15 22:15:17 +0200

    - переименовал raw_data на data / json_data >> parsed_data

--- pprint_json.py
+++ pprint_json.py
@@ -4,19 +4,18 @@ import sys
 
 def load_data(filepath):
     with open(filepath, 'r') as file:
-        parsed_json = json.loads(file.read())
-        return parsed_json
+        return json.loads(file.read())
 
 
-def pretty_print_json(raw_data):
-    pretty_json = json.dumps(raw_data, ensure_ascii=False, indent=4)
+def pretty_print_json(data):
+    pretty_json = json.dumps(data, ensure_ascii=False, indent=4)
     print(pretty_json)
 
 
 if __name__ == '__main__':
     try:
         json_file = sys.argv[1]
-        json_data = load_data(json_file)
+        parsed_data = load_data(json_file)
     except IndexError:
         print('Ошибка! Вы не указали путь к файлу JSON.')
         print('Сработает, если написать "python pprint_json.py <путь к файлу>"')
@@ -26,4 +25,4 @@ if __name__ == '__main__':
     except ValueError:
         print('Ошибка. Файл должен быть в формате JSON.')
     else:
-        pretty_print_json(json_data)
+        pretty_print_json(parsed_data)
